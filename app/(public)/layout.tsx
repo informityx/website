e30b6017b51d320e062c314ee7096eecc7d8509a
Muneeb Arif <muneebarif@Macbooks-MacBook-Pro-2.local>
2026-03-27 22:58:15 +0500

    fix(hero): homepage

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -113,7 +113,7 @@ export default async function PublicLayout({
             navPages={navPages}
             customTypesInHeader={headerCustomTypes}
           />
-          <main className="flex-grow pt-24 lg:pt-20">
+          <main className="flex-grow pt-24 lg:pt-0">
             {children}
             <CTASection />
           </main>
