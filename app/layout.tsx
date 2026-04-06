031d60fde33e84c6901af87cfdda916d3edc52df
Muneeb Arif <muneebarif@Macbooks-MacBook-Pro-2.local>
2026-04-06 22:21:33 +0500

    updated script

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const searchAtlasDynamicOptimizationSrc =
-    "data:text/javascript;base64,dmFyIHNjcmlwdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO3NjcmlwdC5zZXRBdHRyaWJ1dGUoIm5vd3Byb2NrZXQiLCAiIik7c2NyaXB0LnNldEF0dHJpYnV0ZSgibml0cm8tZXhjbHVkZSIsICIiKTtzY3JpcHQuc3JjID0gImh0dHBzOi8vZGFzaGJvYXJkLnNlYXJjaGF0bGFzLmNvbS9zY3JpcHRzL2R5bmFtaWNfb3B0aW1pemF0aW9uLmpzIjtzY3JpcHQuZGF0YXNldC51dWlkID0gIjFkYTI2NzE1LWY3MzctNDhlMy05MzY5LWI2MGFlNDVhMTBkNiI7c2NyaXB0LmlkID0gInNhLWR5bmFtaWMtb3B0aW1pemF0aW9uLWxvYWRlciI7ZG9jdW1lbnQuaGVhZC5hcHBlbmRDaGlsZChzY3JpcHQpOw==";
+    "data:text/javascript;base64,dmFyIHNjcmlwdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO3NjcmlwdC5zZXRBdHRyaWJ1dGUoIm5vd3Byb2NrZXQiLCAiIik7c2NyaXB0LnNldEF0dHJpYnV0ZSgibml0cm8tZXhjbHVkZSIsICIiKTtzY3JpcHQuc3JjID0gImh0dHBzOi8vZGFzaGJvYXJkLnNlYXJjaGF0bGFzLmNvbS9zY3JpcHRzL2R5bmFtaWNfb3B0aW1pemF0aW9uLmpzIjtzY3JpcHQuZGF0YXNldC51dWlkID0gIjYyNDk2YjJkLWVlZjQtNDkwZi04Y2U5LTcxYTEzNWY0ZjZiMyI7c2NyaXB0LmlkID0gInNhLWR5bmFtaWMtb3B0aW1pemF0aW9uLWxvYWRlciI7ZG9jdW1lbnQuaGVhZC5hcHBlbmRDaGlsZChzY3JpcHQpOw==";
 
   return (
     <html lang="en">
@@ -47,7 +47,7 @@ export default function RootLayout({
         <script
           type="text/javascript"
           id="sa-dynamic-optimization"
-          data-uuid="1da26715-f737-48e3-9369-b60ae45a10d6"
+          data-uuid="62496b2d-eef4-490f-8ce9-71a135f4f6b3"
           src={searchAtlasDynamicOptimizationSrc}
           {...{ nowprocket: "", "nitro-exclude": "" }}
         />
